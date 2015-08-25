24fd6d7b5af2a7fe9e59c33b7908afa038d11101
Manuel Woelker <github@manuel.woelker.org>
2015-08-25 16:43:48 +0200

    prettify

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 macro_rules! passert {
     ( $e1:expr, $e2:expr ) => {
         {
@@ -10,22 +11,26 @@ macro_rules! passert {
     };
 }
 
-macro_rules! passert_eq {( $ left : expr , $ right : expr ) => (
-{
-match ( & ( $ left ) , & ( $ right ) ) {
-( left_val , right_val ) => {
-if ! ( * left_val == * right_val ) {
-panic ! (
-"assertion failed: `{} == {}` \
-                           ({}: `{:?}`, {}: `{:?}`)",
-stringify!($ left), stringify!($ right), stringify!($ left), * left_val ,  stringify!($ right),* right_val ) } } } } )
-}
+    macro_rules! passert_eq {
+        ( $ left : expr , $ right : expr ) => (
+            {
+                match ( & ( $ left ) , & ( $ right ) ) {
+                    ( left_val , right_val ) => {
+                        if ! ( * left_val == * right_val ) {
+                            panic ! (
+                                "assertion failed: `{} == {}` ({}: `{:?}`, {}: `{:?}`)",
+                                stringify!($ left), stringify!($ right), stringify!($ left), * left_val ,  stringify!($ right),* right_val )
+                        }
+                    }
+                }
+            }
+        )
+    }
 
 #[test]
 fn it_works() {
     let a = Some(32);
     let b = None;
-
     passert_eq!(a.map(|x| x+3), a);
 //    passert!(a, a);
     passert_eq!(a, b);
