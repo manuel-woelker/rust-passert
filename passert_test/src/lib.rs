87f5e0ab5c4fa53885a818337114514bd6ee8a7c
Manuel Woelker <github@manuel.woelker.org>
2015-08-27 20:59:07 +0200

    fix(passert_test): fix and cleanup passert_test

--- passert_test/src/lib.rs
+++ passert_test/src/lib.rs
@@ -3,23 +3,10 @@
 
 extern crate passert;
 
-fn foo() {
-    let a = 3;
-    let b = -8;
-//    passert!(panic!("testing only  x {:?}", 3));
-//    let mut helper = passert::PassertHelper::new(1234, "XYZT");
-//    helper.add_expression(32, format!("{:?}", a));
-    passert!(a + 2 + 3 != -b);
-//    let msg = String::from("foobar");
-//    passert!(panic!("foo"));
-}
-
 #[test]
+#[should_panic]
 fn it_works() {
-    foo();
-//    let a = 3;
-//    let b = 4;
-//    passert!(a == b);
-//    assert_eq!(rn!(MMXV), 2014);
-//    assert_eq!(rn!(MMXV), 2014);
+    let a = 3;
+    let b = 4;
+    passert!(a + 2 + 3 == -b);
 }
